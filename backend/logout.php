1bfe2ea953c6de8eb95eb442765071c19fe9c472
danpax <danilopacaldojr@gmail.com>
2024-11-26 20:37:11 +0430

    changes

--- backend/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Destroy the session
-session_start();
-$_SESSION['success_message'] = "Logout successfully!";
-header("Location: login.php"); // Redirect to login page
-exit();
-?>
